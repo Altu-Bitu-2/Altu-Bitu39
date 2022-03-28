38db91e729093a151e22191ac845b7b95a13d2da
hyh9339 <100654654+hyh9339@users.noreply.github.com>
2022-03-28 20:55:37 +0900

    Update 11723.cpp

--- [구현&코너케이스] 3월 28일/11723.cpp
+++ [구현&코너케이스] 3월 28일/11723.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,52 +9,46 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int m;
-    set<int> a;
+    int m, x;
+    vector<int> a(20);
 
     cin >> m;
 
     while (m--) {
         string s;
         cin >> s;
-        int x;
 
-        if (s == "add") {
-            cin >> x;
-            if (a.find(x) == a.end())
-                a.insert(x);
-        }
-
-        else if (s == "remove") {
-            cin >> x;
-            if (a.find(x) != a.end())
-                a.erase(x);
-        }
-
-        else if (s == "check") {
-            cin >> x;
-            if (a.find(x) == a.end())
-                cout << 0;
-            else
-                cout << 1;
-        }
-
-        else if (s == "toggle") {
-            cin >> x;
-            if (a.find(x) == a.end())
-                a.insert(x);
-            else
-                a.erase(x);
-        }
-
-        else if (s == "all") {
-            a.clear();
-            for (int i = 0; i < 20; ++i)
-                a.insert(i + 1);
+        if (s == "all") {
+            a.assign(20, true);
+            continue;
         }
 
         else if (s == "empty") {
-            a.clear();
+            a.assign(20, false);
+            continue;
+        }
+
+        cin >> x;
+
+        if (s == "add") {
+            if (!a[x - 1])
+                a[x - 1] = true;
+        }
+
+        else if (s == "remove") {
+            if (a[x - 1])
+                a[x - 1] = false;
+        }
+
+        else if (s == "check") {
+            cout << a[x - 1] << '\n';
+        }
+
+        else if (s == "toggle") {
+            if (a[x - 1])
+                a[x - 1] = false;
+            else
+                a[x - 1] = true;
         }
     }
 
